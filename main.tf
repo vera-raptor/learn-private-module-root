6d0b091052e06f777994bf58a96a4851b76845be
Eden <ang.yanghao97@gmail.com>
2025-04-14 16:50:41 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/dsta-testtest/s3-webapp/aws"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
